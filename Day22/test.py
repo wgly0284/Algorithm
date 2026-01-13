7adf3f2efb2a3100298c4346089b2c02fa718fc4
Hyoji Son <wgly0284@gmail.com>
2026-01-14 07:39:38 +0900

    14Jan26 Stack

---
+++ Day22/test.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(input())
+
+numbers = list(sys.stdin.readline().rsplit())
+
+print(numbers)
+# arr = sys.stdin.readline().rstrip()
+# bomb = sys.stdin.readline().rstrip()
+#
+# stack = []
+#
+# for
