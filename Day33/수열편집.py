2dba4d77e21084a8ab7dd54c6004eec69f5bcd80
손효지 <hyoji0284@naver.com>
2026-03-12 23:42:55 +0900

    수열편집 수정

--- Day33/수열편집.py
+++ Day33/수열편집.py
@@ -35,7 +35,7 @@ for tc in range(1, T + 1):
             idx, val = int(cmd[1]), int(cmd[2])
             # 수정할 인덱스가 현재 리스트 범위 내에 있을 때만 실행
             if 0 <= idx < len(arr):
-                arr[idx] = val
+                arr[idx] = val 
                 
     # 최종 결과 출력
     # L이 리스트의 마지막 인덱스보다 크면 -1 출력
@@ -48,7 +48,8 @@ for tc in range(1, T + 1):
 
 '''
 왜 이전 코드에서 에러가 났을까요?
-인덱스 체크 시점: * result = arr[L] if arr[L] else -1 코드는 arr[L]의 값을 먼저 가져오려고 시도합니다. 만약 L이 10인데 리스트 길이가 5라면, 값을 가져오기도 전에 IndexError가 터져서 프로그램이 멈춰버립니다.
+인덱스 체크 시점: * result = arr[L] if arr[L] else -1 코드는 arr[L]의 값을 먼저 가져오려고 시도합니다.
+만약 L이 10인데 리스트 길이가 5라면, 값을 가져오기도 전에 IndexError가 터져서 프로그램이 멈춰버립니다.
 
 반드시 if L < len(arr): 처럼 인덱스 번호와 리스트의 길이를 먼저 비교해야 합니다.
 
